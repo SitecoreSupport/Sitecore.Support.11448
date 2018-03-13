8746eb739d5ad5e679037bb4413cf2e62ad38b89
ieka <ieka@sitecore.net>
2018-03-13 17:31:02 +0200

    Fix the reason of arising the "Value cannot be null...at Sitecore.XA.Feature.CreativeExchange.Pipelines.Import.Import.AssetEnumeration.Process(ImportArgs args).." exception when importing

---
+++ src/Sitecore.Support.11448/XA/Feature/CreativeExchange/Pipelines/Import/GetImportContext/GetStoragePath.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Configuration;
+using Sitecore.DependencyInjection;
+using Sitecore.XA.Feature.CreativeExchange.Models.Export;
+using Sitecore.XA.Feature.CreativeExchange.Pipelines.Import.GetImportContext;
+using Sitecore.XA.Feature.CreativeExchange.Services;
+
+namespace Sitecore.Support.XA.Feature.CreativeExchange.Pipelines.Import.GetImportContext
+{
+  public class GetStoragePath : GetImportContextBaseProcessor
+  {
+    public override void Process(GetImportContextArgs args)
+    {
+      base.Process(args);
+      string packageName = this.GetParameter("PackageName");
+
+      #region FIX 11448
+      var relativePath = Path.Combine(Settings.PackagePath, @"CreativeExchange", packageName);
+      args.ImportContext.ImportOptions.Storage = ServiceLocator.ServiceProvider.GetService<IStoragePathResolver>().MapPath(relativePath, new HttpContextData(args.HttpContext));
+      #endregion
+    }
+  }
+}
